7bd06cb2f37fb1431b2c5eb4e741838dd1f22731
pippayp <pyappip@gmail.com>
2025-04-27 06:37:53 +0900

    새 블럭 생성 제작중. 파괴는 보류

--- Assets/Script/MapComponent.cs
+++ Assets/Script/MapComponent.cs
@@ -8,7 +8,9 @@ public class MapComponent : MonoBehaviour
     /// <summary>
     /// -1 = 검사되지 않음, 0 = 검사됨, 1 = x축 매치, 2 = y축 매치, 3 = x,y 매치
     /// </summary> 
-    public int[,] MatchData;    
+    public int[,] MatchData;
+
+    private int SpeciesKind = 0;
 
     public void CreateMap(int[,,] levelData, GameObject[] shapes)
     {
@@ -26,6 +28,11 @@ public class MapComponent : MonoBehaviour
                 instance.GetComponent<Block>().species = levelData[0, y, x];
                 instance.transform.position = new Vector2(x, -y);
                 MatchData[y, x] = -1;
+
+                if(SpeciesKind< levelData[0,y,x])
+                {
+                    SpeciesKind = levelData[0, y, x];
+                }
 
                 MapData[y, x] = instance;
             }
@@ -62,5 +69,19 @@ public class MapComponent : MonoBehaviour
                 }
             }
         }
+    }
+
+    //블럭의 종류는 이 스크립트가 가지고있는게 맞지 않을까?
+    //인스턴스의 이름으로도 충분히 어떤 종류의 조각인지 인식할 순 있다.
+    public void CreateBlock(GameObject block, int x, int y)
+    {
+        //createmap의 블럭 생성부분을 가져와 사용, 이 함수로 대채해주자
+        //Random.Range(0, SpeciesKind);
+        GameObject instance = Instantiate(, this.transform);
+        instance.GetComponent<Block>().x = x;
+        instance.GetComponent<Block>().y = y;
+        instance.GetComponent<Block>().species = levelData[0, y, x];
+        instance.transform.position = new Vector2(x, -y);
+        MatchData[y, x] = -1;
     }
 }
